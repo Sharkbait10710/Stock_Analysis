135a2aaa866b3836f05f4072be9678cabc5f4b77
calebwclem <79200015+calebwclem@users.noreply.github.com>
2021-02-24 16:52:10 -0800

    updates to stock.h/.cpp
    Made sure getters were inline functions per assignment req.  Added getStockData per assignent specifications.

--- stock.cpp
+++ stock.cpp
@@ -13,3 +13,15 @@ void print()
 {
 
 }
+
+void setStockInfo(int v, string sym, float highP, float lowP, float openingP,
+	float closingP, float prevP) //Required function to set all of the stock data.
+{
+	volume = v;
+	symbol = sym;
+	todayHigh = highP;
+	todayLow = lowP;
+	openingPrice = openingP;
+	closingPrice = closingP;
+	prevClose = prevP;
+}
--- stock.h
+++ stock.h
@@ -9,7 +9,6 @@ using namespace std;
 
 class stock {
 private:
-	float price;
 	int volume;
 	string symbol;
 	float todayHigh;
@@ -17,56 +16,37 @@ private:
 	float openingPrice;
 	float closingPrice;
 	float prevClose;
-	int perGain;
+	float perGain;
 
 public:
 	stock()
 	{
 		//Default Constructor
-		price = 0;
-		volume = 0;
+		volume = 0.0;
 		symbol = "";
-		todayHigh = 0;
-		todayLow = 0;
-		openingPrice = 0;
-		closingPrice = 0;
-		prevClose = 0;
-		perGain = 0;
+		todayHigh = 0.0;
+		todayLow = 0.0;
+		openingPrice = 0.0;
+		closingPrice = 0.0;
+		prevClose = 0.0;
+		perGain = 0.0;
 	}
 
-	stock(float p, int v, string sym, float highP, float lowP,
-		float openingP,float closingP, float prevP)
-	{
-		//Overloaded Constructor
-		price = p;
-		volume = v;
-		symbol = sym;
-		todayHigh = highP;
-		todayLow = lowP;
-		openingPrice = openingP;
-		closingPrice = closingP;
-		prevClose = prevP;
-	}
+	stock(int volume, string symbol, float highPrice, float lowPrice,
+		float openingPrice, float closingPrice, float prevClose) //Overloaded constructor
+	{setStockInfo(volume, symbol, highPrice, lowPrice, openingPrice, closingPrice, prevClose)}
 
-	float getPrice()
-		{return price;}
-	int getVolume()
-		{return volume;}
-	string getSymbol()
-		{return symbol;}
-	float getTodayHigh()
-		{return todayHigh;}
-	float getTodayLow()
-		{return todayLow;}
-	float getOpeningPrice()
-		{return openingPrice;}
-	float getClosingPrice()
-		{return closingPrice;}
-	float getPrevClose()
-		{return prevClose;}
-	float getPerGain()
-		{return perGain;}
+	void setStockInfo();
+	int getVolume() {return volume;}
+	string getSymbol() {return symbol;}
+	float getTodayHigh() {return todayHigh;}
+	float getTodayLow() {return todayLow;}
+	float getOpeningPrice() {return openingPrice;}
+	float getClosingPrice() {return closingPrice;}
+	float getPrevClose() {return prevClose;}
+	float getPerGain() {return perGain;}
 	void print();
+
 
 };
 
